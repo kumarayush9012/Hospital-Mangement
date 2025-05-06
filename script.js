20f487198445069d194c15394ad5164a53f49eeb
kumarayush9012 <kumar.ayush.sk@gmail.com>
2025-05-06 12:19:52 +0530

    ospital Management project files
    HTML, CSS, JavaScript files for hospital management system.

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSection(sectionId) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+  document.getElementById(sectionId).style.display = 'block';
+}
